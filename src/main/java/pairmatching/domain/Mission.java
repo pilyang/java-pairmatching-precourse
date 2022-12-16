7e982641bdd71ed841d7899fdb80a3bb9133fde5
pilyang <30036534+pilyang@users.noreply.github.com>
2022-12-16 19:16:37 +0900

    feat: Mission 클래스 구현

---
+++ src/main/java/pairmatching/domain/Mission.java
@@ -0,0 +1,12 @@
+package pairmatching.domain;
+
+public class Mission {
+
+    private final String name;
+    private final Level level;
+
+    public Mission(String name, Level level) {
+        this.name = name;
+        this.level = level;
+    }
+}
